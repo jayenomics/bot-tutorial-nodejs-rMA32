981afa4930fc0ad3650871754e57b71330503b62
jayenomics <jay.huhn@gmail.com>
2016-05-03 17:08:04 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ function respond() {
       botRegex = /^\/cool guy/;  botRegexDL = /^\/DDL/i;botRegexSalt = /^\/salt/;botRegexRules = /^\/rules/
       botRegexAd=/^\/advance/;botRegexGTA = /^\/gta/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegexCC = /^\/cc/;
-      botRegexSiege = /^\/siege/; botRegexrattled = /^\/rattled/;
+      botRegexSiege = /^\/siege/; botRegexrattled = /^\/rattled/; botRegexpopcorn = /^\/popcorn/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
@@ -105,6 +105,11 @@ function respond() {
   else if(request.text && botRegexrattled.test(request.text)) {
 this.res.writeHead(200);
 postMessage("http://img3.wikia.nocookie.net/__cb20130515143649/fantendo/images/3/3c/Rattle-me-bones-1-.gif");
+this.res.end();
+}
+  else if(request.text && botRegexpopcorn.test(request.text)) {
+this.res.writeHead(200);
+postMessage("http://i.imgur.com/BCDYv.gif");
 this.res.end();
 }
   
